3bef5172b72c2548b7646cca92b70ce224a4f021
yawaaaa <github1369@gmail.com>
2025-08-17 17:18:27 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-if (!process.env.CI) {
-  var message = '\n\n\n*******\n\nIt looks like you tried to install all-contributors into your project. ' +
-    'This module is simply a specification and doesn\'t actually do anything.\nIf you meant to install the CLI, please ' +
-    'uninstall this module first (npm uninstall all-contributors) and install `all-contributors-cli`.\n\n*******\n\n\n\n'
-  console.error(message)
-  process.exitCode = 1
-}
