009fa31480de13cfc7c33e6f80ec453c99af9161
Felipe Moreno <felipe@flpm.dev>
2025-09-01 10:19:09 -0400

    docs update node version in .nvmrc; remove .astro; fix .gitignore

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/docs/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Foverview.mdx&astroContentModuleFlag=true")]]);
-		
