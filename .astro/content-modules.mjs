ef1289f82915b8f9a078ef4387abe2c5fc93ceac
Felipe Moreno <felipe@flpm.dev>
2025-09-01 09:58:21 -0400

    docs: add astro config

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/docs/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Foverview.mdx&astroContentModuleFlag=true")]]);
+		
