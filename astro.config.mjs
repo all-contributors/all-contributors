9976f618bb1507a709cff8e383b77bdebd93a086
Leah Wasser <leah@pyopensci.org>
2025-09-10 17:45:19 -0600

    docs: style landing page, add branded fonts and colors & tailwind (#901)
    * docs(style): source gfonts poppins
    
    * docs(style): add custom style/ setup tailwind
    
    * docs(img): add and update images
    
    * docs(img): add and update images
    
    * docs(config): cleanup landing and config
    
    * chore(deps): update to support tailwind
    
    * chore(css): setup tailwind css
    
    * docs: add graphics for home page and style buttons
    
    * docs: add buttons
    
    * docs: missed a few more image updates
    
    * chore(deps): correct dependency location in package.lock
    
    * chore(clean): clean old files
    
    * chore: add poppins font license
    
    ---------
    
    Co-authored-by: Jim Madge <jim@jmadge.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,58 +4,70 @@ import starlight from '@astrojs/starlight';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-		site: 'https://allcontributors.org',
-		base: '/',
-		output: 'static',
-		redirects: {
-			// Later we can put redirects here if needed
-		},
-    integrations: [starlight({
-        title: '',
-				logo: {
-					src: './src/assets/logo-full.svg',
-				},
-        favicon: '/icons/favicon-16x16.png',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/all-contributors/all-contributors' }],
-        sidebar: [
-            {
-                label: 'Specification',
-                items: [
-                    { label: 'overview', slug: 'overview' },
-                    { label: 'specification', slug: 'specification' },
-                    { label: 'emoji-key', slug: 'emoji-key' },
-                    { label: 'repository-maintainers', slug: 'repository-maintainers' },
-                    { label: 'tooling', slug: 'tooling' }
-                ],
-            },
-            {
-                label: '@all-contributors Bot 🤖',
-                items: [
-                    { label: 'overview', slug: 'bot/overview' },
-                    { label: 'installation', slug: 'bot/installation' },
-                    { label: 'usage', slug: 'bot/usage' },
-                    { label: 'configuration', slug: 'bot/configuration' },
-                    { label: 'faq', slug: 'bot/faq' }
-                ],
-            },
-            {
-                label: 'CLI',
-                items: [
-                    { label: 'overview', slug: 'cli/overview' },
-                    { label: 'installation', slug: 'cli/installation' },
-                    { label: 'usage', slug: 'cli/usage' },
-                    { label: 'configuration', slug: 'cli/configuration' }
-                ],
-            },
-            {
-                label: 'Project',
-                items: [
-                    { label: 'contribute', slug: 'project/contribute' },
-                    { label: 'code-of-conduct', slug: 'project/code-of-conduct' },
-                    { label: 'implementations', slug: 'project/implementations' }
-                ],
-            },
-        ],
-		}), sitemap()],
+  site: 'https://allcontributors.org',
+  base: '/',
+  output: 'static',
+
+  redirects: {
+      // Later we can put redirects here if needed
+  },
+
+  integrations: [
+      starlight({
+          customCss: [
+              './src/styles/global.css',
+              './src/fonts/font-face.css'],
+          title: '',
+                  logo: {
+                      src: './src/assets/logo-full.svg',
+                  },
+          favicon: '/icons/favicon-16x16.png',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/all-contributors/all-contributors' }],
+          sidebar: [
+              {
+                  label: 'Specification',
+                  items: [
+                      { label: 'overview', slug: 'overview' },
+                      { label: 'specification', slug: 'specification' },
+                      { label: 'emoji-key', slug: 'emoji-key' },
+                      { label: 'repository-maintainers', slug: 'repository-maintainers' },
+                      { label: 'tooling', slug: 'tooling' }
+                  ],
+              },
+              {
+                  label: '@all-contributors Bot 🤖',
+                  items: [
+                      { label: 'overview', slug: 'bot/overview' },
+                      { label: 'installation', slug: 'bot/installation' },
+                      { label: 'usage', slug: 'bot/usage' },
+                      { label: 'configuration', slug: 'bot/configuration' },
+                      { label: 'faq', slug: 'bot/faq' }
+                  ],
+              },
+              {
+                  label: 'CLI',
+                  items: [
+                      { label: 'overview', slug: 'cli/overview' },
+                      { label: 'installation', slug: 'cli/installation' },
+                      { label: 'usage', slug: 'cli/usage' },
+                      { label: 'configuration', slug: 'cli/configuration' }
+                  ],
+              },
+              {
+                  label: 'Project',
+                  items: [
+                      { label: 'contribute', slug: 'project/contribute' },
+                      { label: 'code-of-conduct', slug: 'project/code-of-conduct' },
+                      { label: 'implementations', slug: 'project/implementations' }
+                  ],
+              },
+          ],
+          }), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
